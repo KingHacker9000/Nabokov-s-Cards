453f169fe55d2cb400e78e88427e5e77e656f698
Ashish Ajin Thomas <Coolioboss7@gmail.com>
2024-10-03 23:36:45 -0400

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, session, send_file, redirect, jsonify
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+# Create Flask Application
+app = Flask(__name__)
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Home Directory
+@app.route('/')
+def home():
+    return render_template("index.html")
+ 
+@app.route('/chat', methods=["GET", 'POST'])
+def chat():
+    
+    # POST Method
+    if request.method == "POST":
+        text = request.form.get("text")
+    else:
+        text = request.args.get("text")
+        if text is None:
+            return "Error"
+
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {
+            "role": "assistant",
+            "content": "Speak like a very creative writer, following is a conversation, continue it:" + text
+        }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    reply = completion.choices[0].message.content
+    print(reply)
+    if request.method == "POST":
+        return render_template("chat.html", text=reply)
+    return reply
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
